c41ebe41625603ee7658cfc2eceed99218848156
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-28 21:54:57 -0300

    Criacao do programa no arquivo program

--- CalcularSalario/Program.cs
+++ CalcularSalario/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using CalcularSalario.Entities;
+using System.Globalization;
 
 namespace CalcularSalario
 {
@@ -6,7 +9,39 @@ namespace CalcularSalario
     {
         static void Main(string[] args)
         {
-            
+            List<Employee> list = new List<Employee>();
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CI);
+                if (ch == 'y')
+                {
+                    Console.Write("Additional Charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CI);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CI));
+            }
         }
     }
 }
